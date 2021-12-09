f0e2113efd5938df48c929dc5e54b0a4c4d3a0ec
movie0world <63150363+movie0world@users.noreply.github.com>
2021-12-09 16:51:29 +0500

    commit

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(function (req, res, next) {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(process.cwd(), "public")));
+app.use(express.static("public"));
 
 // ================= Model & Connection ==================================
 require("./Model");
