555667903d939e5f59d363b7a20b83c4d01735b9
movie0world <63150363+movie0world@users.noreply.github.com>
2021-12-09 13:42:37 +0500

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const axios = require("axios");
 const bcrypt = require("bcrypt");
 const { detect, detectOS } = require("detect-browser");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const { v4 } = require("uuid");
 
